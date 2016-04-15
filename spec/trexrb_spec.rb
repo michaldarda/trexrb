8487d5c8f9d1deb2befaf3e7eca57edffa05104f
Michał Darda <michaldarda@gmail.com>
2016-04-15 23:20:52 +0200

    Extract helper mock_socket method

--- spec/trexrb_spec.rb
+++ spec/trexrb_spec.rb
@@ -4,48 +4,46 @@ describe "Trexrb functional spec" do
   let(:backend) { Trexrb::Backend.new }
 
   it "returns empty list of keys when is empty" do
-    expect(Socket).to receive(:tcp).with('localhost', 4040)
-      .and_return(fake_socket)
+    mock_socket
 
     expect(backend.store.keys).to eq([])
   end
 
   it "doesn't fail when accessing missing key" do
-    expect(Socket).to receive(:tcp).with('localhost', 4040)
-      .and_return(fake_socket)
+    mock_socket
 
     expect(backend.store[:foo]).to eq(nil)
   end
 
   it "sets key to value" do
-    expect(Socket).to receive(:tcp).with('localhost', 4040)
-      .and_return(fake_socket "OK\r\n")
+    mock_socket "OK\r\n"
 
     expect(backend.store["foo"] = "bar").to eq "bar"
   end
 
   it "gets single key" do
-    expect(Socket).to receive(:tcp).with('localhost', 4040)
-      .and_return(fake_socket "bar\r\n")
+    mock_socket "bar\r\n"
 
     expect(backend.store["foo"]).to eq "bar"
   end
 
   it "lists all available keys" do
-    expect(Socket).to receive(:tcp).with('localhost', 4040)
-      .and_return(fake_socket "foo,goo\r\n")
+    mock_socket("foo,goo\r\n")
 
     expect(backend.store.keys).to eq("foo,goo")
   end
 
   private
 
-  def fake_socket(response = "\r\n")
-    double().tap do |fake_socket|
+  def mock_socket(response = "\r\n")
+    fake_socket = double().tap do |fake_socket|
       expect(fake_socket).to receive(:close_write)
       expect(fake_socket).to receive(:print)
       expect(fake_socket).to receive(:read)
         .and_return(response)
     end
+
+    expect(Socket).to receive(:tcp).with('localhost', 4040)
+      .and_return(fake_socket)
   end
 end
